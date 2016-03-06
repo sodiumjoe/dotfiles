ac902b3e9c7cca7a4dc75ee43d570c79ab045204
Joe Moon <joe@xoxomoon.com>
2016-03-05 22:38:27 -0800

    neovim

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,13 +10,28 @@ git clone --recursive https://github.com/sodiumjoe/prezto.git ~/.zprezto
 
 git clone https://github.com/sodiumjoe/nvm.git ~/.nvm
 
-symlink dotfiles
+# symlink dotfiles
 
 files=("mackup.cfg" "agignore" "gitconfig" "tmux.conf" "vimrc")
 
 for file in ${files[@]}; do
   ln -s ~/.dotfiles/${file} ~/.${file}
 done
+
+# neovim
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+
+# python 3 for neovim for deoplete
+pip3 install neovim
+
+# update deoplete
+nvim +UpdateRemotePlugins +qall
+
+# vim-plug
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # http://brew.sh/
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
