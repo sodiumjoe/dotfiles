07708449326a2e27d15f1d21b5cf29aa51efb859
Joe Moon <joe@xoxomoon.com>
2016-03-05 16:45:02 -0800

    ask for password up front

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 git clone --recursive https://github.com/sodiumjoe/prezto.git ~/.zprezto
 
 git clone https://github.com/sodiumjoe/nvm.git ~/.nvm
