9116ee31cb0b129eb1653f23b5af6c9b275bd720
Joe Moon <joe@xoxomoon.com>
2016-12-29 23:15:05 -0500

    mackup backjup

--- Mackup/.hammerspoon/init.lua
+++ Mackup/.hammerspoon/init.lua
@@ -1,24 +1,34 @@
 hs.window.animationDuration = 0
 hs.grid.setMargins({ 0, 0 })
-local log = hs.logger.new('foo', 5)
+-- local log = hs.logger.new('foo', 5)
 local space=hs.window.filter.new(nil,'space'):setCurrentSpace(true):setDefaultFilter{}
 
 local LEFT = 'Left'
 local RIGHT = 'Right'
 local UP = 'Up'
 
+function isMaximized(f, screen, max)
+  local primary = hs.screen.primaryScreen()
+  local isPrimary = primary:id() == screen:id()
+  if isPrimary then
+    return (f.x == max.x - 4) and f.y == max.y and f.w == max.w + 4 and f.h == max.h
+  else
+    return f.x == max.x and f.y == max.y and f.w == max.w and f.h == max.h
+  end
+end
+
 function isPushed(dir, f, max)
   if not f.h == max.h then return false end
   if dir == LEFT then return f.x == max.x and f.w == (max.w / 2) end
-  if dir == RIGHT then return f.x == (max.x + (max.w / 2)) and f.w == (max.w / 2) end
+  if dir == RIGHT then return (f.x == (max.x + (max.w / 2)) or f.x == (max.x + (max.w/2 - 4))) and (f.w == (max.w / 2) or f.w == (max.w / 2 + 4)) end
 end
 
 function throw(dir, win)
   if dir == LEFT then
-    win:moveOneScreenWest(100)
+    win:moveOneScreenWest()
     split(RIGHT)
   else
-    win:moveOneScreenEast(100)
+    win:moveOneScreenEast()
     split(LEFT)
   end
 end
@@ -30,7 +40,7 @@ function push(dir)
   local screen = win:screen()
   local max = screen:frame()
 
-  if isPushed(dir, f, max) then
+  if isPushed(dir, f, max) and not isMaximized(f, screen, max) then
     throw(dir, win)
   else
     split(dir)
@@ -43,6 +53,7 @@ function split(dir)
   local screen = win:screen()
   local max = screen:frame()
   local primary = hs.screen.primaryScreen()
+  local isMultipleScreens = #hs.screen.allScreens() > 1
   local isPrimary = primary:id() == screen:id()
 
   if dir == LEFT then
@@ -54,7 +65,13 @@ function split(dir)
       f.w = max.w / 2
     end
   else
-    f.x = max.x + max.w / 2
+    if isPrimary and isMultipleScreens then
+      f.x = max.x + max.w / 2 - 4
+      f.w = max.w / 2
+    else
+      f.x = max.x + max.w / 2
+      f.w = max.w / 2
+    end
   end
 
   f.y = max.y
@@ -78,10 +95,19 @@ function maximize()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
-  f.x = max.x - 4
-  f.y = max.y
-  f.w = max.w + 4
-  f.h = max.h
+  local primary = hs.screen.primaryScreen()
+  local isPrimary = primary:id() == screen:id()
+  if isPrimary then
+    f.x = max.x - 4
+    f.y = max.y
+    f.w = max.w + 4
+    f.h = max.h
+  else
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h
+  end
   win:setFrame(f)
 end
 
