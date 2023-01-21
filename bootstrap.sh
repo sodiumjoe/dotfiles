2b383a45f460144bb9e4a3b17eac8de701c48a13
Joe Moon <joe@xoxomoon.com>
2023-01-20 23:18:43 -0800

    set xdg home if not set

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+export XDG_CONFIG_HOME = ${XDG_CONFIG_HOME:=$HOME/.config}
+mkdir -p ${XDG_CONFIG_HOME}
+
 git clone --recursive git@github.com:zimfw/zimfw.git ${XDG_CONFIG_HOME}/zsh/.zim
 
 # symlink dotfiles
@@ -34,8 +37,6 @@ xdg_files=(\
   "karabiner"\
   "vivid"\
   )
-
-mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 
 for file in ${xdg_files[@]}; do
   ln -s ~/.dotfiles/${file} ${XDG_CONFIG_HOME}/${file}
