abd492cf92d82134f98242655f950bdd72690b24
Joe Moon <joe@xoxomoon.com>
2023-03-25 17:27:50 -0700

    treesitter-playground

--- neovim/lua/sodium/plugins.lua
+++ neovim/lua/sodium/plugins.lua
@@ -759,6 +759,30 @@ require("lazy").setup({
 		},
 	},
 	{
+		"nvim-treesitter/playground",
+		opts = {
+			playground = {
+				enable = true,
+				disable = {},
+				updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+				persist_queries = false, -- Whether the query persists across vim sessions
+				keybindings = {
+					toggle_query_editor = "o",
+					toggle_hl_groups = "i",
+					toggle_injected_languages = "t",
+					toggle_anonymous_nodes = "a",
+					toggle_language_display = "I",
+					focus_language = "f",
+					unfocus_language = "F",
+					update = "R",
+					goto_node = "<cr>",
+					show_help = "?",
+				},
+			},
+		},
+		cmd = { "TSPlaygroundToggle" },
+	},
+	{
 		"nvim-treesitter/nvim-treesitter-context",
 		config = function()
 			require("treesitter-context").setup()
