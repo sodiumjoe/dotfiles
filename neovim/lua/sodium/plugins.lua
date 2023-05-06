b4bf82ab7d4c412b11b94909da1d82dd5fd30535
Joe Moon <joe@xoxomoon.com>
2023-05-05 22:11:30 -0700

    formatting

--- neovim/lua/sodium/plugins.lua
+++ neovim/lua/sodium/plugins.lua
@@ -430,8 +430,10 @@ require("lazy").setup({
 			end
 
 			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, vim.g.popup_opts)
-			vim.lsp.handlers["textDocument/signatureHelp"] =
-				vim.lsp.with(vim.lsp.handlers.signature_help, vim.g.popup_opts)
+			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+				vim.lsp.handlers.signature_help,
+				vim.g.popup_opts
+			)
 
 			local on_attach = function(client)
 				lsp_status.on_attach(client)
