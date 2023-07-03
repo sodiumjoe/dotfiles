b45c910afa5eed58a4c5ece94e79d1037ec63592
Joe Moon <joe@xoxomoon.com>
2023-07-03 13:41:58 -0700

    disable tsserver formatting

--- neovim/lua/sodium/plugins.lua
+++ neovim/lua/sodium/plugins.lua
@@ -800,7 +800,14 @@ require("lazy").setup({
 	{
 		"pmizio/typescript-tools.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		opts = {},
+		config = function()
+			require("typescript-tools").setup({
+				on_attach = function(client)
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+				end,
+			})
+		end,
 	},
 	"rhysd/conflict-marker.vim",
 	{
