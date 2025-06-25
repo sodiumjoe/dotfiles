1af6214a8e36f3d925cec69507b5a84658acacff
Joe Moon <joe@xoxomoon.com>
2025-06-25 16:32:10 -0700

    focused borders

---
+++ hammerspoon/Spoons/highlight_focused_window.spoon/init.lua
@@ -0,0 +1,54 @@
+local obj = {}
+
+local borderColor = { red = 1, green = 1, blue = 1, alpha = 0.5 }
+local borderWidth = 2
+local borderRadius = 10
+
+local focusBorder = nil
+
+local function deleteBorder()
+    if focusBorder then
+        focusBorder:delete()
+        focusBorder = nil
+    end
+end
+
+local function drawBorder()
+    local win = hs.window.focusedWindow()
+
+    deleteBorder()
+
+    if not win then
+        return
+    end
+
+    local frame = win:frame()
+
+    if focusBorder then
+        focusBorder:setFrame(frame)
+    else
+        focusBorder = hs.canvas.new(frame)
+        focusBorder[1] = {
+            type = "rectangle",
+            action = "stroke",
+            roundedRectRadii = { xRadius = borderRadius, yRadius = borderRadius },
+            strokeColor = borderColor,
+            strokeWidth = borderWidth,
+        }
+        focusBorder:show()
+    end
+end
+
+hs.window.filter.new():subscribe({
+    hs.window.filter.windowFocused,
+    hs.window.filter.windowMoved,
+    hs.window.filter.windowUnminimized,
+    hs.window.filter.windowUnhidden,
+}, drawBorder):subscribe({
+    hs.window.filter.windowUnfocused,
+    hs.window.filter.windowDestroyed,
+    hs.window.filter.windowMinimized,
+    hs.window.filter.windowHidden,
+}, deleteBorder)
+
+return obj
